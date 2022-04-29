141b613f39cc2bfabdeec72dc3661211a213e740
Christina Fernandes <fernandes.christ14@gmail.com>
2022-04-29 15:37:54 -0300

    Screen connections placement plus prime numbers calculation code

--- app/src/main/java/com/example/numero_primo/MainActivity.java
+++ app/src/main/java/com/example/numero_primo/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.numero_primo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonCalcular;
+    private EditText respostaUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCalcular = findViewById(R.id.btnCalcular);
+        respostaUser = findViewById(R.id.resptUser);
+
+        buttonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Resultado.class);
+
+                int contador = 0;
+                String resultadoUser = "";
+                int respostaUserNumerica = Integer.parseInt(respostaUser.getText().toString());
+
+                if (respostaUserNumerica >= 1) {
+                    for (int i = 1; i <= respostaUserNumerica; i++) {
+                        double resultado = respostaUserNumerica % i;
+                        if (resultado == 0) {
+                            contador = contador + 1;
+                        }
+                    }
+                }
+
+                if (contador == 2) {
+                    resultadoUser = "Seu número é Primo";
+                } else {
+                    resultadoUser = "Seu número não é Primo";
+
+                }
+                intent.putExtra("resultado", resultadoUser);
+                startActivity(intent);
+                Log.d("AQUIIIIIIII", resultadoUser);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/numero_primo/Resultado.java
+++ app/src/main/java/com/example/numero_primo/Resultado.java
@@ -1,5 +1,23 @@
 package com.example.numero_primo;
 
-public class Resultado {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Resultado extends AppCompatActivity {
+    private TextView resultadoFinal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resultado);
+
+        Intent intent = getIntent();
+        String dadosRecebidos = intent.getStringExtra("resultado");
+
+        resultadoFinal = findViewById(R.id.textResposta);
+        resultadoFinal.setText(dadosRecebidos);
+    }
 }
